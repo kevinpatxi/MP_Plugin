3b8a1ac6a149aa8a6f3ed01bf5b09452dbcdd29e
kevinpatxi <kevin.patxi@gmail.com>
2017-04-19 11:02:51 -0700

    Initial push

---
+++ MP_Plugin/PluginClass.cs
@@ -0,0 +1,51 @@
+﻿/*
+This is a sample plugin frame work
+ 
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MissionPlanner;
+
+namespace MP_Plugin
+{
+    public class PluginClass : MissionPlanner.Plugin.Plugin
+    {
+        public override string Name
+        {
+            get { return "Plugin Name"; }
+        }
+
+        public override string Version
+        {
+            get { return System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString(); }
+        }
+
+        public override string Author
+        {
+            get { return "Author Name"; }
+        }
+
+        public override bool Init()
+        {
+            return true;
+        }
+
+        public override bool Loaded()
+        {
+            MissionPlanner.Plugin.PluginHost Host = new MissionPlanner.Plugin.PluginHost();
+            PluginForm Plugin_UI = new PluginForm(Host);
+            Plugin_UI.Show();
+            return true;
+        }
+
+
+        public override bool Exit()
+        {
+            return true;
+        }
+    }
+}
---
+++ MP_Plugin/PluginForm.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MP_Plugin
+{
+    public partial class PluginForm : Form
+    {
+        MissionPlanner.Plugin.PluginHost MP_Host;
+
+        public PluginForm(MissionPlanner.Plugin.PluginHost Host)
+        {
+            this.MP_Host = Host;
+            InitializeComponent();
+        }
+    }
+}
